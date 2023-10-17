4c5386733916a606ef19fcda954e85211fc08769
Ryan Hood <ryanhood@ryans-mbp.lan>
2023-10-16 19:40:49 -0500

    worked on JSON file, imported some pictures

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is cloud hosting my small, json database for my airbnb application 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View Application 
         </a>
       </header>
     </div>
