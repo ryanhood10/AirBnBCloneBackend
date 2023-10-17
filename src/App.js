231bb91010b753cb90445a024c7373b222072862
Ryan Hood <ryanhood@ryans-mbp.lan>
2023-10-16 20:59:39 -0500

    can call data correctly internally

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data/data';
 
 function App() {
   return (
@@ -7,15 +9,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is cloud hosting my small, json database for my airbnb application 
+          This is cloud hosting my small, JSON database for my Airbnb application
         </p>
+        <div className="data-list">
+          {data.map((item, index) => (
+            <div key={index} className="data-item">
+              <img src={item.img} alt={item.title} />
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <p>Rating: {item.star}</p>
+              <p>Price: {item.price}</p>
+            </div>
+          ))}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          View Application 
+          View Application
         </a>
       </header>
     </div>
---
+++ src/server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+
+// Serve your React app's build folder (assuming you've built it)
+app.use(express.static('build'));
+
+// Define your API endpoint to serve data
+app.get('/api/data', (req, res) => {
+  // Import your data.js file
+  const { data } = require('./src/data/data');
+
+  // Return the data as JSON
+  res.json(data);
+});
+
+// Start the server on a specified port (e.g., 3001)
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
